6db29d111673be7a6c48b4f44356cdf1de02db87
Roman <rome.gun@gmail.com>
2013-04-05 15:01:01 -0400

    Lose  all the things

--- student.rb
+++ student.rb
@@ -23,58 +23,84 @@ class Instructor < Person
   attr_accessor :type
 end
 
-@directory = []
-puts "Student Directory, v0.0.2 by WDI"
-print "Enter Student or Instructor, pretty l to load & q to save and quit: "
+begin
+  Person.open_database "student_directory.db"
+  puts "Student Directory, v0.0.3 by WDI & R"
+  print 'Enter Student or Instructor, p to print, s to search, q to quit: '
 
-while ((input = gets.strip.chomp) != 'q') do
+  while ((input = gets.strip.chomp) != 'q') do
 
+    case input
+    when 'p'
+      # Find all the people who are in the database 
+      # and print out their information
 
-  case input
-  when "Student" 
-    person = Student.new
-    
-    print "What is your name? "
-    person.name = gets.strip.chomp
-    print "What is your email? "
-    person.email = gets.strip.chomp
-    print "Have you ever been skydiving? "
-    person.skydiving = gets.strip.chomp
-    print "Tell me something about yourself :: "
-    person.fun_fact = gets.strip.chomp
-    print "What is your reason for joining GA? "
-    person.reason_for_joining = gets.strip.chomp
-    
-  when 'Instructor'
-    person = Instructor.new
+    when 's'
+      # Ask the user to enter a search term
+      print "Please enter the search term that you're looking for: "
+      search = gets.strip.chomp
 
-    print "What is your name? "
-    person.name = gets.strip.chomp
-    print "What is your email? "
-    person.email = gets.strip.chomp
-    print "Have you ever been skydiving? "
-    person.skydiving = gets.strip.chomp
-    print "Tell me something about yourself :: "
-    person.fun_fact = gets.strip.chomp
-    print "What is your reason for joining GA? "
-    person.reason_for_joining = gets.strip.chomp
-    print "What sort of instructor are you? "
-    person.type = gets.strip.chomp
+      # Find all the people who's name matches the given string
+      # and print out their information
 
-  when 'l'
-    @directory << YAML.load_documents(File.open('student_directory.yml')) {|f| f}
-    
+    else
+      # Create either a Student or Instructor object, depending on the input
+      person = Person.create_person(input)
+
+      unless person.nil?
+        # Prompt the user for information about a Person
+        person.ask_questions
+
+        # Save someone in our database
+        person.save
+      end 
+    end
   end
-  
-  # Append this to our yaml file
-  @directory << person
-  #binding.pry
-  
+
+  def to_s
+    "ID: #{self.id}"
+    def save
+    # Build a String of SQL, that will insert all the attributes into the persons table
+    sql = "insert into people ('name', ) values (?,?,?,?)"
+    # Execute the SQL on the @@db object
+  end
+end
+rescue Exception => e
+  puts "Error ! #{e}"
+  puts "#{e.backtrace.join "\n"}"
+ensure
+  # Ensure that the database is closed before we go
+  Person.close_database
 end
 
-# Open a student_directory.yml YAML file and write it out on one line
-File.open('student_directory.yml', 'a') { |f|
- @directory.compact.each do |person|
-f.write(person.to_yaml) 
-end 
-} 
+case input
+when "Student" 
+  person = Student.new
+
+  print "What is your name? "
+  person.name = gets.strip.chomp
+  print "What is your email? "
+  person.email = gets.strip.chomp
+  print "Have you ever been skydiving? "
+  person.skydiving = gets.strip.chomp
+  print "Tell me something about yourself :: "
+  person.fun_fact = gets.strip.chomp
+  print "What is your reason for joining GA? "
+  person.reason_for_joining = gets.strip.chomp
+
+when 'Instructor'
+  person = Instructor.new
+
+  print "What is your name? "
+  person.name = gets.strip.chomp
+  print "What is your email? "
+  person.email = gets.strip.chomp
+  print "Have you ever been skydiving? "
+  person.skydiving = gets.strip.chomp
+  print "Tell me something about yourself :: "
+  person.fun_fact = gets.strip.chomp
+  print "What is your reason for joining GA? "
+  person.reason_for_joining = gets.strip.chomp
+  print "What sort of instructor are you? "
+  person.type = gets.strip.chomp
+end
